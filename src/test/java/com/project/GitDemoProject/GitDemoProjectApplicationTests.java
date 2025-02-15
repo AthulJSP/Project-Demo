d70f539488ea22bbe2368d845ff5bc75835074ef
AthulJSP <athulshetty907@gmail.com>
2025-02-15 15:38:34 +0530

    Git added

---
+++ src/test/java/com/project/GitDemoProject/GitDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.GitDemoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
