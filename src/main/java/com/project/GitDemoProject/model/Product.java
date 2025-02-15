09c6c23009e2e2c86f5bc0b7a753ebe76f3767eb
AthulJSP <athulshetty907@gmail.com>
2025-02-15 16:54:44 +0530

    adding Product Entity

---
+++ src/main/java/com/project/GitDemoProject/model/Product.java
@@ -0,0 +1,40 @@
+package com.project.GitDemoProject.model;
+
+public class Product {
+    private int productId;
+    private String productName;
+    private String brand;
+    private String productPrice;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
+}
